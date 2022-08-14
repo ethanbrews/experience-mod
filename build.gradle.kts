fc38d3dbcaa7bbdce322ca0bb588496ea01112d6
Ethan Brews <efyeb3@nottingham.ac.uk>
2022-08-14 22:50:26 +0100

    Create project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,37 @@ plugins {
     id("fabric-loom")
     val kotlinVersion: String by System.getProperties()
     kotlin("jvm").version(kotlinVersion)
+    id("com.modrinth.minotaur").version("2.+")
 }
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("experience")
+    versionNumber.set("1.0.0")
+    versionType.set("alpha")
+    //uploadFile.set(tasks.remapJar as (Provider<out Any>)) // With Loom, this MUST be set to `remapJar` instead of `jar`!
+    gameVersions.addAll(listOf("1.19.2"))
+    loaders.add("fabric")
+    dependencies {
+        // The scope can be `required`, `optional`, `incompatible`, or `embedded`
+        required.project("fabric-api")
+        required.project("fabric-language-kotlin")
+    }
+}
+
 base {
     val archivesBaseName: String by project
     archivesName.set(archivesBaseName)
 }
+
+
 val modVersion: String by project
 version = modVersion
 val mavenGroup: String by project
 group = mavenGroup
 repositories {}
+
+
 dependencies {
     val minecraftVersion: String by project
     minecraft("com.mojang", "minecraft", minecraftVersion)
@@ -24,6 +45,8 @@ dependencies {
     val fabricKotlinVersion: String by project
     modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion)
 }
+
+
 tasks {
     val javaVersion = JavaVersion.VERSION_17
     withType<JavaCompile> {
